5f56ac6a20f401234ad9d3d56c36cacc8f425019
1passionfruit <mightyfinething@yahoo.com>
2023-04-10 17:41:00 -0500

    Concept transfer from example. Need to investigate solve()

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  cachedMatrix<-NULL ##to check if matrix inverse already in object
+  
+  set <- function(newMatrixInput){
+    x<<-newMatrixInput
+    cachedMatrix<<-NULL
+  }
+  
+  get<- function() x ##return newMatrix
+  
+  setInverse<-function(inverse) cachedMatrix<<-inverse
+  getInverse<-function() cacheMatrix
+  list(set=set, get=get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
@@ -11,5 +26,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+    inverseMatrix<-x$getInverse()
+    if(!is.null(inverseMatrix)){
+      message("getting cached matrix")
+      return(inverseMatrix)
+    }
+    matrixData<-x$get()
+    inverseMatrix <-solve(matrixData)
+    x$setInverse(inverseMatrix)
+    inverseMatrix
         ## Return a matrix that is the inverse of 'x'
-}
+ }
