790a3de02fd1a44ead61503c71c7c70c9de2c21c
1passionfruit <mightyfinething@yahoo.com>
2023-04-10 20:42:05 -0500

    very few substantial changes, just more comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,20 +2,23 @@
 ## functions do
 
 ## Write a short comment describing this function
+##function defines 4 functions for x and stores a cached Inverse of x
 
 makeCacheMatrix <- function(x = matrix()) {
   
-  cachedMatrix<-NULL ##to check if matrix inverse already in object
+  cachedMatrix<-NULL ##cached matrix
   
-  set <- function(newMatrixInput){
+  ##funtions to mutate/access 
+  set <- function(newMatrixInput){ ##set inputMatrix
     x<<-newMatrixInput
     cachedMatrix<<-NULL
   }
   
-  get<- function() x ##return newMatrix
+  get<- function() x ##return InputMatrix
   
+  ##functions to mutate/access the inverse
   setInverse<-function(inverse) cachedMatrix<<-inverse
-  getInverse<-function() cacheMatrix
+  getInverse<-function() cachedMatrix
   list(set=set, get=get,
        setInverse = setInverse,
        getInverse = getInverse)
@@ -24,14 +27,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+##function to receive a matrix, compute its inverse if needed (no existing
+## inverse or if matrix has changed)
 cacheSolve <- function(x, ...) {
     inverseMatrix<-x$getInverse()
-    if(!is.null(inverseMatrix)){
+    if(!is.null(inverseMatrix)){ ##if is.Null() is False, matrix already cached
       message("getting cached matrix")
       return(inverseMatrix)
     }
-    matrixData<-x$get()
+    matrixData<-x$get() ##does this really do anything??
     inverseMatrix <-solve(matrixData)
     x$setInverse(inverseMatrix)
     inverseMatrix
